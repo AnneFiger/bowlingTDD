05a45094da5e3783ea9033b14ba97364bdfdf406
Anne Figer <anne-laure.figer@madetech.com>
2022-02-23 15:37:16 +0000

    created new test taking 3 as input, passed test, refactored and created test for a single strike

--- bowling.rb
+++ bowling.rb
@@ -1,7 +1,11 @@
 class Bowling
 	def score(rolls)
-		return nil if rolls.nil?
-
-		0
+		if rolls.nil? 
+		    return nil
+		elsif rolls == ''
+			return 0
+		else
+			return rolls.to_i
+		end
 	end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -8,6 +8,16 @@ describe 'bowling score' do
 
 	it 'returns 0 if given no rolls' do
 		bowling = Bowling.new
-		expect(bowling.score([])).to be_zero
+		expect(bowling.score("")).to be_zero
+	end
+
+	it 'returns 3 if given "3"' do
+		bowling = Bowling.new
+		expect(bowling.score("3")).to eq(3)
+	end
+
+	it 'returns 10 if given "X"' do
+		bowling = Bowling.new
+		expect(bowling.score("X")).to eq(10)
 	end
 end
